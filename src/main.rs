75c64c19b4379e91031d034769292554992397f2
Abu Sakib <mabusakib@gmail.com>
2023-11-04 17:48:24 +0600

    Prepare for integration tests: separate logic into a library

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
-
-/// Handler for the `/heath_check` endpoint.
-/// Returns a `200 OK` response with no body.
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
+async fn main() -> Result<(), std::io::Error> {
+   run()
     .await
 }
