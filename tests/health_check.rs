2f47c8cffc8b14f139eb344f3b94635c04b0def7
Abu Sakib <mabusakib@gmail.com>
2023-11-05 21:06:36 +0600

    Initial implementation of capturing form data for the `/subcriptions` endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -21,6 +21,65 @@ async fn health_check_works() {
     assert_eq!(response.content_length(), Some(0));
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Spawn our app and take the address it's running on
+    let app_url = spawn_app();
+
+    // Create a client using reqwest
+    let client = reqwest::Client::new();
+
+    // The body of the POST request
+    let body = "name=fernando%20pessoa&email=fernando_pessoa%40gmail.com";
+
+    // Use the client to interact with the server and
+    // get the response from the `/health_check` endpoint
+    let response = client
+        .get(&format!("{}/subscriptions", &app_url))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert!(response.status().is_success());
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_for_invalid_form_data() {
+    // Spawn our app and take the address it's running on
+    let app_url = spawn_app();
+
+    // Create a client using reqwest
+    let client = reqwest::Client::new();
+
+    // The body of the POST request containing invalid form data testcases
+    let test_cases = vec![
+        ("name=fernando%20pessoa", "email is missing"),
+        ("email=fernando_pessoa%40gmail.com", "name is missing"),
+        ("", "missing both name and email"),
+    ]; 
+
+    for (invalid_body, err_msg) in test_cases {
+        let response = client
+            .get(&format!("{}/subscriptions", &app_url))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Custom error message on test failure
+            "The API did not fail with a 400 Bad Request when the payload was {}",
+            err_msg
+        );
+    }
+}
+
 // Launch the server in the background for testing.
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
